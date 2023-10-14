f482373c1e8d19b1c068d4ee0afb0fc819730177
Kevin Xu <k3vn.xu@gmail.com>
2023-10-14 00:08:27 -0400

    simplify index

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
@@ -36,20 +36,6 @@ const Blog: React.FC<Props> = (props) => {
           ))}
         </main>
       </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
     </Layout>
   )
 }
