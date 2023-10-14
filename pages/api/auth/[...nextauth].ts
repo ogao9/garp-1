c163baceed509dbdb65a2ebf4acabc82b01b60af
Oliver Gao <ogao@umich.edu>
2023-10-14 08:53:59 -0400

    change prisma adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
 import prisma from '../../../lib/prisma';
 
