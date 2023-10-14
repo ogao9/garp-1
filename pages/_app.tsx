1067cc4d91a3ecc32ad36c19765930c7aee2a51a
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2023-10-14 02:35:59 -0400

    add nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '/styles/globals.css';
+import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <NextUIProvider>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </NextUIProvider>
   );
 };
 
